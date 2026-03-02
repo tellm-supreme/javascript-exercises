bb30225b1652205064f0971a47e05a0436c3b86e
tellm-supreme <tellmsupreme04@gmail.com>
2026-03-02 15:17:05 +0100

    Finished 05_joinStrings foundations.
    Had to use concatenation to join strings
    created variables with const and assigned values
    - to them.
    Arranged those variables in a sentence manner.

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,6 +9,14 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const age = 18;
+const fullName = "Carlos Stevenson";
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age + " years old."
 
 
 
